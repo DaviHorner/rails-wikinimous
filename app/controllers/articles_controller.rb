96d398529b2a2c16fb33dcb251f23409597e4c06
Davi Horner <davihhcastro@gmail.com>
2016-08-10 11:18:18 -0300

    Add

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,38 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(params_article)
+    @article.save
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(params_article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  end
+
+  private
+
+  def params_article
+    params.require(:article).permit(:title, :content)
+  end
+end
